b7f3f137002235247b8c3ceaab55b493dc03a334
Hetav21 <shahhetav2106@gmail.com>
2025-12-26 13:03:57 +0530

    feat: add China wallpaper as desktop background

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -7,14 +7,17 @@ import { WindowManager } from '../window/WindowManager';
 
 export const Desktop = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden bg-gradient-to-br from-[#1a1b26] via-[#2d2e3b] to-[#3b2d4a]">
-      {/* Background Image / Gradient - NixOS Theme */}
-      <div className="absolute inset-0 pointer-events-none opacity-50 bg-[url('/nixos-logo.svg')] bg-no-repeat bg-center bg-contain opacity-5 mix-blend-overlay" />
+    <div className="relative h-screen w-screen overflow-hidden">
+      {/* Background Wallpaper */}
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/wallpaper.jpeg')" }}
+      />
       
       {/* Desktop Shell */}
       <TopBar />
       
-      {/* Window Manager Area (Z-0 to allow windows to be below topbar/dock if needed, but usually windows are between) */}
+      {/* Window Manager Area */}
       <div className="absolute inset-0 pt-8 pb-20 z-0">
         <WindowManager />
       </div>
